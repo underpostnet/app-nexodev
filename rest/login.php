25e2e101b77473dc58a75cef1f3ae96bdd03a152
fcoverdugoa <fcoverdugoa@underpost.net>
2020-12-06 22:13:51 -0400

    Init Login System

--- rest/login.php
+++ rest/login.php
@@ -1,11 +1,41 @@
 <?php
 
+include 'C:/xampp/htdocs/nexodev/modules/util.php';
+
+$data = json_decode(file_get_contents('C:/xampp/htdocs/nexodev/draft/json/nexodev_db.json'), true);
 
 $json_res = json_decode("{}", true);
 
-$params = json_decode( file_get_contents( 'php://input' ), true );
+$input = json_decode( file_get_contents( 'php://input' ), true );
 
-echo json_encode($params);
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
+
+
+$json_res['state'] = false;
+
+for($i=0;$i<l($data['clientes']);$i++){
+
+  if(( $data['clientes'][$i]['email'] == $input['email'] )
+    and
+    ( $data['clientes'][$i]['pass'] == $input['pass'] )){
+
+      $json_res['state'] = true;
+      $json_res['content'] = $data['clientes'][$i];
+
+  }
+
+}
+
+
+
+
+
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+echo json_encode($json_res);
 
  ?>
